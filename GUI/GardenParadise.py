7005b75970f9df7821ed7f20956b1c8b32b09d92
Joaquin Garcia <joaquing@myvcs.ca>
2024-12-06 18:33:39 +0900

    finished draft of show_selected_plant

--- GUI/GardenParadise.py
+++ GUI/GardenParadise.py
@@ -370,4 +370,234 @@ def show_selected_plant(plant_id):
         climate_zone = Label(maintenance_info_frame, text="Climate Zone", font=("Arial", 13, "bold"), fg="black", bg=right_frame["bg"]) #make any commas separated with a space
         climate_zone.grid(row=0, column=0, padx=10, pady=5, sticky="w")  # Title in the first row
 
-        climate_zone_label = Label(mai
+        climate_zone_label = Label(maintenance_info_frame, text=f"{plant_details[3].replace(',', ', ')}", font=("Arial", 12), fg="black", bg=right_frame["bg"], wraplength=180, justify="left")
+        climate_zone_label.grid(row=1, column=0, padx=10, pady=5, sticky="w")  # Info label underneath
+
+        # Row 2 - "Water Needs" title and info label
+        water_needs = Label(maintenance_info_frame, text="Water Needs", font=("Arial", 13, "bold"), fg="black", bg=right_frame["bg"])
+        water_needs.grid(row=0, column=1, padx=10, pady=5, sticky="w")  # Title in the first row
+
+        water_needs_label = Label(maintenance_info_frame, text=f"{plant_details[5]}", font=("Arial", 12), fg="black", bg=right_frame["bg"])
+        water_needs_label.grid(row=1, column=1, padx=10, pady=5, sticky="w")  # Info label underneath
+
+        # Row 3 - "Light Needs" title and info label
+        light_needs = Label(maintenance_info_frame, text="Light Needs", font=("Arial", 13, "bold"), fg="black", bg=right_frame["bg"])
+        light_needs.grid(row=0, column=2, padx=10, pady=5, sticky="w")  # Title in the first row
+
+        light_needs_label = Label(maintenance_info_frame, text=f"{plant_details[6]}", font=("Arial", 12), fg="black", bg=right_frame["bg"])
+        light_needs_label.grid(row=1, column=2, padx=10, pady=5, sticky="w")  # Info label underneath
+
+        # Row 4 - "Soil Type" title and info label
+        soil_type = Label(maintenance_info_frame, text="Soil Type", font=("Arial", 13, "bold"), fg="black", bg=right_frame["bg"])
+        soil_type.grid(row=2, column=0, padx=10, pady=5, sticky="w")  # Title in the first row
+
+        soil_type_label = Label(maintenance_info_frame, text=f"{plant_details[7].replace(',', ', ')}", font=("Arial", 12), fg="black", bg=right_frame["bg"])
+        soil_type_label.grid(row=3, column=0, padx=10, pady=5, sticky="w")  # Info label underneath
+
+        # Row 5 - "Maintenance Level" title and info label
+        maintenance = Label(maintenance_info_frame, text="Maintenance Level", font=("Arial", 13, "bold"), fg="black", bg=right_frame["bg"])
+        maintenance.grid(row=2, column=1, padx=10, pady=5, sticky="w")  # Title in the first row
+
+        maintenance_label = Label(maintenance_info_frame, text=f"{plant_details[8]}", font=("Arial", 12), fg="black", bg=right_frame["bg"])
+        maintenance_label.grid(row=3, column=1, padx=10, pady=5, sticky="w")  # Info label underneath
+
+        # Create the bottom frame for "Plant Characteristics"
+        bottom_frame = Frame(right_frame, bg="grey", bd=2, relief="solid")  # Border with grey background
+        bottom_frame.pack(fill="x", padx=10, pady=10, expand=False)
+
+        # Add the label to the bottom frame
+        bottom_label = Label(bottom_frame, text="Plant Characteristics", font=("Arial", 14, "bold"), bg="grey")
+        bottom_label.pack(pady=5, padx=10)
+
+        #plant characteristics frame
+        pc_frame = Frame(right_frame, bg=right_frame["bg"])
+        pc_frame.pack(fill="x", padx=10, pady=10)
+
+        # Row 1 - "Flower Colour" Title and info label
+        flower_color = Label(pc_frame, text=f"Flower Color", font=("Arial", 13, "bold"), fg="black", bg=right_frame["bg"])
+        flower_color.grid(row=0, column=0, padx=10, pady=5, sticky="w")  # Title in the first row
+        
+        flower_color_label = Label(pc_frame, text=f"{plant_details[4]}", font=("Arial", 12), fg="black", bg=right_frame["bg"])
+        flower_color_label.grid(row=1, column=0, padx=10, pady=5, sticky="w")  # Info label underneath
+
+        # Row 2 - "Foliage Color" title and info label
+        foliage_color = Label(pc_frame, text="Foliage Color", font=("Arial", 13, "bold"), fg="black", bg=right_frame["bg"])
+        foliage_color.grid(row=0, column=1, padx=10, pady=5, sticky="w")  # Title in the first row
+
+        foliage_color_label = Label(pc_frame, text=f"{plant_details[9]}", font=("Arial", 12), fg="black", bg=right_frame["bg"])
+        foliage_color_label.grid(row=1, column=1, padx=10, pady=5, sticky="w")  # Info label underneath
+
+        # Row 3 - "Perfume" title and info label
+        perfume = Label(pc_frame, text="Perfume", font=("Arial", 13, "bold"), fg="black", bg=right_frame["bg"])
+        perfume.grid(row=0, column=2, padx=10, pady=5, sticky="w")  # Title in the first row
+
+        perfume_label = Label(pc_frame, text=f"{plant_details[10]}", font=("Arial", 12), fg="black", bg=right_frame["bg"])
+        perfume_label.grid(row=1, column=2, padx=10, pady=5, sticky="w")  # Info label underneath
+
+        # Row 4 - "Aromatic" title and info label
+        aromatic = Label(pc_frame, text="Aromatic", font=("Arial", 13, "bold"), fg="black", bg=right_frame["bg"])
+        aromatic.grid(row=2, column=0, padx=10, pady=5, sticky="w")  # Title in the first row
+
+        aromatic_label = Label(pc_frame, text=f"{plant_details[11].replace(',', ', ')}", font=("Arial", 12), fg="black", bg=right_frame["bg"])
+        aromatic_label.grid(row=3, column=0, padx=10, pady=5, sticky="w")  # Info label underneath
+
+        # Row 5 - "Edible" title and info label
+        edible = Label(pc_frame, text="Edible", font=("Arial", 13, "bold"), fg="black", bg=right_frame["bg"])
+        edible.grid(row=2, column=1, padx=10, pady=5, sticky="w")  # Title in the first row
+
+        edible_label = Label(pc_frame, text=f"{plant_details[12]}", font=("Arial", 12), fg="black", bg=right_frame["bg"])
+        edible_label.grid(row=3, column=1, padx=10, pady=5, sticky="w")  # Info label underneath
+
+        # Notes Frame
+        notes = Frame(right_frame, bg="grey", bd=2, relief="solid")
+        notes.pack(fill="x", padx=10, pady=10, expand=False)
+
+        # Add the label to the top frame
+        notes_label = Label(notes, text="Notes:", font=("Arial", 14, "bold"), bg="grey")
+        notes_label.pack(pady=5, padx=(10, 0), side="left")
+
+        if plant_details[13]:
+            notes_text = plant_details[13]
+
+            text_width = len(notes_text)
+            max_width = 50
+
+            if text_width > max_width:
+                notes_info = Label(notes, text="Click to view full notes", font=("Arial", 14, "italic"), fg="blue", bg="grey", cursor="hand2")
+                notes_info.pack(pady=5, padx=10, side="left")
+
+                def show_full_notes():
+                    full_notes_window = Toplevel()
+                    full_notes_window.title(f"{plant_details[0]} Notes")
+
+                    full_notes_window.resizable(True, True)
+                    
+                    full_notes_frame = Frame(full_notes_window, bg="grey")
+                    full_notes_frame.pack(fill=BOTH, expand=TRUE)
+
+                    notes_label = Label(full_notes_frame, text="Notes:", font=("Arial", 14, "bold"), anchor="w", bg="grey", justify="left")
+                    notes_label.pack(padx=10, pady=(10, 0), fill="x")
+                    
+                    # Create the label with text wrapping and justification
+                    full_notes_label = Label(full_notes_frame, 
+                                            text=notes_text, 
+                                            font=("Arial", 14), 
+                                            wraplength=380, 
+                                            justify="left", 
+                                            bg="grey", 
+                                            anchor="w")  # 'anchor="w"' aligns the text to the left
+                    
+                    # Add padding around the label
+                    full_notes_label.pack(padx=10, pady=(0, 10), fill=BOTH, expand=True)
+                
+                notes_info.bind("<Button-1>", lambda e: show_full_notes())
+            
+            else:
+                # If the text fits, just show it normally
+                notes_info = Label(notes, text=notes_text, font=("Arial", 14), bg="grey")
+                notes_info.pack(pady=5, padx=0, side="left")
+        else:
+            # If there are no notes, display a default message
+            no_info = Label(notes, text="No extra notes", font=("Arial", 14), bg="grey")
+            no_info.pack(pady=5, side="left")
+       
+    else:
+        print("No details found for this plant.")
+
+
+#GUI function that displays the plants after search in scrollable_frame
+def show_plants(plants):
+
+    if not plants:
+        label = Label(scrollable_frame, text="No products were found matching your selection.", font=("Arial", 12), bg="white")
+        label.place(relx=0.5, rely=0.2, anchor="center")
+        return
+    else:
+        for row_idx, (common_name, botanical_name, plant_id) in enumerate(plants):
+            column_count = row_idx % 3
+            plant_frame = Frame(scrollable_frame, bg="lightgray", relief=SOLID, borderwidth=1)
+            plant_frame.grid(row=row_idx // 3, column=column_count, padx=10, pady=10, sticky="nsew")
+
+            # Image
+            image = Image.open(r"/Users/joaquingarcia/Documents/31305 Relational Databases/GardenParadise/Data/cute-pot.png")
+            resize_image = image.resize((80, 80))  # Resize to a larger size if 10x10 is too small
+            img = ImageTk.PhotoImage(resize_image)  # Use ImageTk.PhotoImage, not PhotoImage
+
+            # Add the image to a Label
+            image_label = Label(plant_frame, image=img, bg="lightgray")
+            image_label.image = img  #Keep a reference to prevent garbage collection
+            image_label.pack(anchor="center", padx=5, pady=5)
+                
+            # Display common name
+            common_label = Label(plant_frame, text=f"{common_name}", font=("Arial", 12, "bold"), bg="lightgray")
+            common_label.pack(anchor="center", padx=5, pady=2)
+
+            # Display botanical name
+            botanical_label = Label(plant_frame, text=f"{botanical_name}", font=("Arial", 12, "italic"), bg="lightgray")
+            botanical_label.pack(anchor="center", padx=5, pady=2)
+
+            # More info Button
+            more_info_button = Button(plant_frame, text="More info", font=("Arial", 8), command=lambda plant_id=plant_id: show_selected_plant(plant_id))
+            more_info_button.pack(anchor="center", padx=5, pady=10)
+
+
+#Clear existing widgets in the scrollable frame and entry
+def clear_entry_and_frame():
+    for widget in scrollable_frame.winfo_children():
+        widget.destroy()
+    search_entry.delete(0, 'end')
+
+def clear_frame():
+    for widget in scrollable_frame.winfo_children():
+        widget.destroy()
+    for i in range(len(dropdown_labels)):
+        dropdown_options[i].set("")
+
+def clear_all_search(): 
+    clear_entry_and_frame() 
+    for i in range(len(dropdown_labels)):
+        dropdown_options[i].set("")
+
+#Showing advanced search results
+def show_advanced_search():
+
+    clear_entry_and_frame()
+    plants = fetch_plant_results()
+    show_plants(plants)
+
+def show_entry_search(event=None):
+
+    clear_frame()
+    plant_name = search_entry.get().strip()
+    plants = get_plants_by_search(plant_name)
+    show_plants(plants)
+
+#Find plant search button
+find_plant_button = Button(search_frame, text="Find plant", font=("Arial", 12), command=show_advanced_search)
+find_plant_button.grid(padx=10, pady=10)
+
+clear_button = Button(search_frame, text="Clear all", font=("Arial", 12, "bold"), bg="#06402B", fg="white", command=clear_all_search)
+clear_button.place(relx=0.5, rely=0.95, anchor="s")
+
+#Search plant button
+search_button = Button(search_frame, text="Search", font=("Arial", 12), command=show_entry_search)
+search_button.grid(row=1, column=0, padx=(10, 10), pady=10)  # Place next to entry
+search_entry.bind('<Return>', show_entry_search)
+
+
+
+window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
