bfee9c07e85708a2b250b4b5ed7797365084ac7b
Joaquin Garcia <joaquing@myvcs.ca>
2024-12-08 21:28:30 +0900

    committing to pull

---
+++ Database/plants_table.sql
@@ -0,0 +1,65 @@
+
+/*
+
+Our Garden Paradise Database
+Creation!
+
+*/
+USE garden_paradise;
+
+########################### CREATING PLANTS TABLE #########################
+
+#Create plants table
+CREATE TABLE plants (
+	id INT,
+    `Plant ID` INT NOT NULL AUTO_INCREMENT,
+    `Plant Code` TEXT,
+    `Botanical Name` TEXT,
+    `Common Name` TEXT,
+    `Previous Name` TEXT,
+    `Plant Type` TEXT,
+    `Water Needs` TEXT,
+    `Climate Zones` TEXT,
+    `Light Needs` TEXT,
+    `Soil Type` TEXT,
+    `Soil Additional` TEXT,
+    `Maintenance` TEXT,
+    `Abcission` TEXT,
+    `Height Ranges` TEXT,
+    `Spread Ranges` TEXT,
+    `Flower Colour` TEXT,
+    `Foliage Colour` TEXT,
+    `Perfume` TEXT,
+    `Aromatic` TEXT,
+    `Edible` TEXT,
+    `Bird Attracting` TEXT,
+    `Bird Attractant` TEXT,
+    `Bore Water Tolerance` TEXT,
+    `Frost Tolerance` TEXT,
+    `Greywater Tolerance` TEXT,
+    `Native` TEXT,
+    `Butterfly Attracting` TEXT,
+    `Butterfly Type` TEXT,
+    `Image` TEXT,
+    `Image Location` TEXT,
+    `Image Owner` TEXT,
+    `Herb External Have` TEXT,
+    `Herb Images Change To` TEXT,
+    `Notes` TEXT,
+    `Why Photo Removed` TEXT,
+    `Why Plant Removed` TEXT,
+    `Actioned By` TEXT,
+    `Date Actioned` TEXT,
+    `Status` TEXT,
+    PRIMARY KEY (`Plant ID`)
+);
+
+#Edited out because it didn' work. Used Load Data Import Wizard instead
+/*
+LOAD DATA LOCAL INFILE '/path/waterwise-plants.csv'
+INTO TABLE garden_paradise.plants
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+*/
---
+++ Database/users_table.sql
@@ -0,0 +1,25 @@
+################################# CREATING USERS TABLE #####################################
+
+# Create USER table
+CREATE TABLE IF NOT EXISTS users (
+	user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    `password` VARCHAR(255) NOT NULL
+    );
+    
+# Alter users table add first_name
+ALTER TABLE users ADD COLUMN `first_name` VARCHAR(50) NOT NULL AFTER username;
+
+# Alter users table add last_name
+ALTER TABLE users ADD COLUMN `last_name` VARCHAR(50) NOT NULL AFTER first_name;
+
+# Sample "new user" insert query
+INSERT INTO users (username, first_name, last_name, `password`)
+	VALUES ('joaquinbgarcia', 'Joaquin', 'Garcia', 'testing');
+    
+UPDATE users SET first_name = "Joaquin" WHERE username = "joaquinbgarcia" AND `password` = "testing";
+UPDATE users SET last_name = "Garcia" WHERE username = "joaquinbgarcia" AND `password` = "testing";
+    
+# Sample user retrieval
+SELECT * FROM users WHERE username = "joaquinbgarcia";
+SELECT * FROM users;
